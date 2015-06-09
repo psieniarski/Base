4bb7c92be643c80fc6526aa3a18607f063a0918b
Pawel Sieniarski <paabson@gmail.com>
2015-06-10 01:40:46 +0200

    add helperss

--- Base.js
+++ Base.js
@@ -7,3 +7,28 @@
 */
 
 var $ = require('jquery');
+
+var helper = {
+    once: function(fn, cx) { 
+        var result;
+
+        return function() { 
+            if(fn) {
+                result = fn.apply(cx || this, arguments);
+                fn = null;
+            }
+
+            return result;
+        };
+    },
+
+    camelCase: function() {
+        var args = [].slice.call(arguments);
+
+        for(var i = 1; i < args.length; i++) {
+            args[i] = args[i].charAt(0).toUpperCase() + args[i].slice(1);
+        }
+
+        return args.join('');
+    },
+};
