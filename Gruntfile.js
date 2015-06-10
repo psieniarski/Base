597abc19c14f5d859a223fefdf1b3a324f531340
Pawel Sieniarski <paabson@gmail.com>
2015-06-10 21:21:45 +0200

    added copy task to grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,13 +20,25 @@ module.exports = function(grunt) {
           'build/Jaskier.js': ['src/Jaskier.js'],
         }
       }
+    },
+
+    copy: {
+      vendor: {
+        files: [
+          // includes files within path
+          {expand: true, src: ['bower_components/mocha/*'], dest: 'vendor/mocha'},
+          {expand: true, src: ['bower_components/chai/*'], dest: 'vendor/chai'},
+          {expand: true, src: ['node_modules/jquery/dist/*'], dest: 'vendor/jquery'},
+        ],
+      },
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-notify');
 
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['copy', 'watch']);
 };
