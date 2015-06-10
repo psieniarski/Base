bb4193d038ac60b14aa2716e3b6b30d5768963c5
Pawel Sieniarski <paabson@gmail.com>
2015-06-10 20:58:32 +0200

    added Universal Module Deifinition

--- src/Jaskier.js
+++ src/Jaskier.js
@@ -6,116 +6,137 @@
  * http://www.opensource.org/licenses/MIT
 */
 
-var $ = require('jquery');
+// Universal Module Definition
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('jquery'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.b);
+    }
+}(this, function ($) {
+    
+    var helper = {
+        once: function(fn, cx) { 
+            var result;
 
-var helper = {
-    once: function(fn, cx) { 
-        var result;
+            return function() { 
+                if(fn) {
+                    result = fn.apply(cx || this, arguments);
+                    fn = null;
+                }
 
-        return function() { 
-            if(fn) {
-                result = fn.apply(cx || this, arguments);
-                fn = null;
+                return result;
+            };
+        },
+
+        camelCase: function() {
+            var args = [].slice.call(arguments);
+
+            for(var i = 1; i < args.length; i++) {
+                args[i] = args[i].charAt(0).toUpperCase() + args[i].slice(1);
             }
 
-            return result;
-        };
-    },
-
-    camelCase: function() {
-        var args = [].slice.call(arguments);
-
-        for(var i = 1; i < args.length; i++) {
-            args[i] = args[i].charAt(0).toUpperCase() + args[i].slice(1);
-        }
-
-        return args.join('');
-    },
-};
-
-Jaskier.extend = function(obj) {
-    var that = this;
-    var Child = function() {
-        that.apply(this, arguments); 
+            return args.join('');
+        },
     };
 
-    Child.prototype = $.extend({}, that.prototype, obj);
-
-    return Child;
-};
-
-Jaskier.prototype = $.extend({}, helper, {
-    currentBreakpoint: '',
-
-    _prepareUI: function() {
-        var ui = $.extend({}, this.ui);
-        var newui = this.ui = {};  
-        
-        $.each(ui, function(name, selector) {
-            selector = (selector == 'window') ? window : selector;
-            newui[name] = $(selector); 
-        });
-    },
-
-    _bindEvents: function() {
-        var that = this; 
-        $.each(this.events, function(str, funcname) {
-            var arr = str.split(' ');
-            var e = arr.shift();
-            var selector = arr.join(' ');
-
-            selector = (selector == 'window') ? window : selector;
-
-            if (selector[0] === '@') {
-                selector = that.ui[selector.slice(1)];
-            }  
-
-            if (that.el) {
-                selector = that.$el.find(selector);
-            } else {
-                selector = $(selector);
-            }
-
-            selector.on(e, that[funcname].bind(that));
-        });
-    },
-
-    _prepareBreakpoints: function() {
+    Jaskier.extend = function(obj) {
         var that = this;
-        var camelCase = this.camelCase;
-        var breakpoints = this.breakpoints; 
-        var bp = [];
+        var Child = function() {
+            that.apply(this, arguments); 
+        };
 
-        function match(mq, cb1, cb2, name) {
-            if (mq.matches) {
-                that.currentBreakpoint = name;
-                if (cb1) {
-                    cb1.apply(that, mq);
-                }
-            } else {
-                if (that.currentBreakpoint === name) {
-                    that.currentBreakpoint = '';
+        Child.prototype = $.extend({}, that.prototype, obj);
+
+        return Child;
+    };
+
+    Jaskier.prototype = $.extend({}, helper, {
+        currentBreakpoint: '',
+
+        _prepareUI: function() {
+            var ui = $.extend({}, this.ui);
+            var newui = this.ui = {};  
+            
+            $.each(ui, function(name, selector) {
+                selector = (selector == 'window') ? window : selector;
+                newui[name] = $(selector); 
+            });
+        },
+
+        _bindEvents: function() {
+            var that = this; 
+
+            $.each(this.events, function(str, funcname) {
+                var arr = str.split(' ');
+                var e = arr.shift();
+                var selector = arr.join(' ');
+
+                selector = (selector == 'window') ? window : selector;
+
+                if (selector[0] === '@') {
+                    selector = that.ui[selector.slice(1)];
+                }  
+
+                if (that.el) {
+                    selector = that.$el.find(selector);
+                } else {
+                    selector = $(selector);
                 }
 
-                if (cb2) {
-                    cb2.apply(that, mq);
+                selector.on(e, that[funcname].bind(that));
+            });
+        },
+
+        _prepareBreakpoints: function() {
+            var that = this;
+            var camelCase = this.camelCase;
+            var breakpoints = this.breakpoints; 
+            var bp = [];
+
+            function match(mq, cb1, cb2, name) {
+                if (mq.matches) {
+                    that.currentBreakpoint = name;
+                    if (cb1) {
+                        cb1.apply(that, mq);
+                    }
+                } else {
+                    if (that.currentBreakpoint === name) {
+                        that.currentBreakpoint = '';
+                    }
+
+                    if (cb2) {
+                        cb2.apply(that, mq);
+                    }
                 }
             }
-        }
 
-        $.each(breakpoints, function(name, breakpoint) {
-            var mq = window.matchMedia(breakpoint);     
+            $.each(breakpoints, function(name, breakpoint) {
+                var mq = window.matchMedia(breakpoint);     
 
-            mq.addListener(function() {
+                mq.addListener(function() {
+                    match(mq, that[name], that[camelCase('not', name)], name);
+                }); 
+
                 match(mq, that[name], that[camelCase('not', name)], name);
-            }); 
+            });
+        }
+    });
 
-            match(mq, that[name], that[camelCase('not', name)], name);
-        });
-    }
-});
+    // Just return a value to define the module export.
+    return {
+        Jaskier: Jaskier
+    };
+}));
 
-module.exports = {
-    Jaskier: Jaskier
-}; 
+
+
+
 
